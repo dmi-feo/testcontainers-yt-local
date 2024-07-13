7d0391c95217fa9f641b1a75f04e1af8f09ae739
Dmitriy Fedorov <fdi1992@gmail.com>
2024-07-13 13:45:36 +0200

    add pytest fixture (#12)

--- tests/test_yt_local.py
+++ tests/test_yt_local.py
@@ -4,16 +4,15 @@ from testcontainers_yt_local.container import YtLocalContainer
 
 
 def test_docker_run_yt():
-    yt_container = YtLocalContainer()
-    with yt_container as yt:
-        url = f"http://{yt.get_container_host_ip()}:{yt.get_exposed_port(YtLocalContainer.PORT_HTTP)}/ping"
+    with YtLocalContainer() as yt:
+        url = f"{yt.proxy_url_http}/ping"
         r = requests.get(url)
         assert r.status_code == 200
 
 
 def test_list_root_node():
     with YtLocalContainer() as yt:
-        url = f"http://{yt.get_container_host_ip()}:{yt.get_exposed_port(YtLocalContainer.PORT_HTTP)}/api/v3/list"
+        url = f"{yt.proxy_url_http}/api/v3/list"
         r = requests.get(url, params={"path": "/"})
         assert r.status_code == 200
         assert set(r.json()) == {"home", "sys", "tmp", "trash"}
@@ -22,12 +21,18 @@ def test_list_root_node():
 def test_two_containers():
     with YtLocalContainer() as yt1, YtLocalContainer() as yt2:
         for yt in (yt1, yt2):
-            url = f"http://{yt.get_container_host_ip()}:{yt.get_exposed_port(YtLocalContainer.PORT_HTTP)}/ping"
+            url = f"{yt.proxy_url_http}/ping"
             r = requests.get(url)
             assert r.status_code == 200
 
 
-def test_config_override():
+def test_yt_client_config_override():
     with YtLocalContainer() as yt:
         yt_cli = yt.get_client(config={"prefix": "//tmp"})
         assert yt_cli.config["prefix"] == "//tmp"
+
+
+def test_with_fixture(yt_cluster_function):
+    url = f"{yt_cluster_function.proxy_url_http}/ping"
+    r = requests.get(url)
+    assert r.status_code == 200
