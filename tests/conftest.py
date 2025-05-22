a614df264cff437066841e26bf0a9af81cb3779b
Dmitriy Fedorov <fdi1992@gmail.com>
2025-05-22 23:59:28 +0200

    better test parametrization [changelog-ignore]

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ import pytest
 pytest_plugins = ["testcontainers_yt_local"]
 
 
-@pytest.fixture(scope="session", params=[False, True])
+@pytest.fixture(scope="function", params=[False, True], ids=["default", "ng"])
 def use_ng_image(request):
     return request.param
